cddb11faf591defba6f352c092739f0f5778d655
mundo <brunoserrao19@gmail.com>
2021-06-15 23:26:26 +0100

    Add defaults to downloadFromCallback Fix audio not playing due to https recaptcha by using base64

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //@ts-nocheck
 /**
  * @typedef {(
+    *  "en"
     *  "af-ZA"|"am-ET"|"hy-AM"|"az-AZ"|"id-ID"|"ms-MY"|"bn-BD"|"bn-IN"|"ca-ES"|"cs-CZ"|"da-DK"|"de-DE"|
     *  "en-AU"|"en-CA"|"en-GH"|"en-GB"|"en-IN"|"en-IE"|"en-KE"|"en-NZ"|"en-NG"|"en-PH"|"en-SG"|"en-ZA"|
     *  "en-TZ"|"en-US"|"es-AR"|"es-BO"|"es-CL"|"es-CO"|"es-CR"|"es-EC"|"es-SV"|"es-ES"|"es-US"|"es-GT"|
@@ -13,11 +14,28 @@
     *  "ar-QA"|"ar-LB"|"ar-EG"|"fa-IR"|"hi-IN"|"th-TH"|"ko-KR"|"zh-TW"|"yue-Hant-HK"|"ja-JP"|"zh-HK"|"zh"
     *  )} Language
 */
-
-const googleTTS = require('google-tts-api');
+const googleTTS = require('google-tts-api'); // CommonJS
 const fs = require('fs');
-const https = require('https');
 const Stream = require('stream');
+
+function base64ToBinary(base64Text){
+  const binary=Buffer.from(base64Text,"base64").toString("binary");
+  const buffer=new ArrayBuffer(binary.length);
+  let bytes=new Uint8Array(buffer);
+  let i=0;
+  const bytesLength=buffer.byteLength;
+  for (i; i < bytesLength; i++) {
+      bytes[i]=binary.charCodeAt(i) & 0xFF;
+  }
+  return bytes;
+}
+
+function base64toBinaryStream(base64Text){
+  const binary=base64ToBinary(base64Text);
+  const stream=new Stream.PassThrough();
+  stream.write(binary,"binary");
+  return stream;
+}
 
 /**
  * @param {string} text
@@ -28,30 +46,11 @@ const Stream = require('stream');
  * @param {number} cfg.timeout
  * @param {string} cfg.splitPunct
  */
-function downloadFromInfoCallback(stream, text, {
-  lang, slow, host, timeout, splitPunct
-}) {
-    const urlInfo = googleTTS.getAllAudioUrls(text, {
-      lang, slow, host, timeout, splitPunct
-    })
-    Promise.all(
-      urlInfo.map(({url}) => {
-        return new Promise((resolve, reject) => {
-          https.get(url, (response, err) => {
-            if (err) {
-              reject(err);
-              return;
-            }
-            resolve(response);
-          });
-        });
-      })
-    ).then((responses) => {
-      // Once obtained, perform in order
-      responses.forEach((response) => {
-        response.pipe(stream);
-      });
-    });
+function downloadFromInfoCallback(stream, text, {lang, slow, host, timeout, splitPunct}) {
+    googleTTS.getAudioBase64(text, {lang, slow, host, timeout, splitPunct})
+      .then(base64Audio => base64toBinaryStream(base64Audio))
+      .then(audioStream => audioStream.pipe(stream))
+      .catch(console.error);
 }
 
 /**
@@ -62,13 +61,9 @@ function downloadFromInfoCallback(stream, text, {
  * @param {number} cfg.timeout
  * @param {string} cfg.splitPunct
  */
-function getVoiceStream(text, {
-  lang = 'en-GB', slow = false, host, timeout, splitPunct
-} = {}) {
+function getVoiceStream(text, {lang = 'en', slow = false, host = 'https://translate.google.com', timeout = 10000, splitPunct} = {}) {
     const stream = new Stream.PassThrough();
-    downloadFromInfoCallback(stream, text, {
-      lang, slow, host, timeout, splitPunct
-    });
+    downloadFromInfoCallback(stream, text, {lang, slow, host, timeout, splitPunct });
     return stream;
 }
 
@@ -82,14 +77,10 @@ function getVoiceStream(text, {
  * @param {number} cfg.timeout
  * @param {string} cfg.splitPunct
  */
-function saveToFile(filePath, text, {
-  lang = 'en-GB', slow = false, host, timeout, splitPunct
-} = {}) {
+function saveToFile(filePath, text, {lang = 'en-GB', slow = false, host, timeout, splitPunct} = {}) {
     const stream = new Stream.PassThrough();
     const writeStream = fs.createWriteStream(filePath);
-    downloadFromInfoCallback(stream, text, {
-      lang, slow, host, timeout, splitPunct
-    });
+    downloadFromInfoCallback(stream, text, {lang, slow, host, timeout, splitPunct });
     stream.pipe(writeStream);
     stream.on('end', () => writeStream.close());
 }
